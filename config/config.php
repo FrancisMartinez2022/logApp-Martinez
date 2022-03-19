f9cea5ddd4b468539c0ebd8da852ae1608aa9370
FrancisMartinez2022 <201840137@psu.palawan.edu.ph>
2022-03-20 00:08:35 +0800

    done config

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-	define('ROOT_URL', 'http://localhost/phpsandbox/logApp-scaling-octo/');
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'logapp');
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6480126');
+	define('DB_PASS', 'hPfjFYlbXH');
+	define('DB_NAME', 'sql6480126');
